590290c0031aebb13ede0b2ae2aa75ff908677ab
FlorianBruijn <florian@bruijn.eu>
2023-11-24 20:25:21 +0100

    muchBetterPerlinTerrain

--- Assets/scripts/mesh/callMesh.cs
+++ Assets/scripts/mesh/callMesh.cs
@@ -7,12 +7,16 @@ public class callMesh : MonoBehaviour
     [SerializeField] private int Xlen;
     [SerializeField] private int Ylen;
     [SerializeField] private int seed;
+    [SerializeField] private float scale;
+    [SerializeField] private int octaves;
+    [SerializeField] private float percistance;
+    [SerializeField] private float lacunarity;
     [SerializeField] private MeshFilter filter;
     [SerializeField] private float change;
     void Start()
     {
         //float[,] heightmap = waveFunction_v1.elevation(Xlen, Ylen, change);
-        float[,] heightmap = perlinNiose.heightMap(Xlen, Ylen, seed);
+        float[,] heightmap = perlinNiose.heightMap(Xlen, Ylen, seed, scale, octaves, percistance, lacunarity);
         MeshData meshData = makeMesh.generateMesh(heightmap);
 
         filter.sharedMesh = meshData.createMesh();
--- Assets/scripts/terrainGenerationV2/perlinNiose.cs
+++ Assets/scripts/terrainGenerationV2/perlinNiose.cs
@@ -5,15 +5,47 @@ using Random = UnityEngine.Random;
 
 public static class perlinNiose
 {
-    public static float[,] heightMap(int width, int height, int seed)
+    public static float[,] heightMap(int width, int height, int seed, float scale, int octaves, float percistance, float lacunarity)
     {
-        Random.seed = seed;
         float[,] toReturn = new float[width, height];
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                toReturn[x, y] = Mathf.PerlinNoise(x * Random.value, y * Random.value);
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleX = x / scale * frequency;
+                    float sampleY = y / scale * frequency;
+
+                    float prelinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += prelinValue * amplitude;
+
+                    amplitude *= percistance;
+                    frequency *= lacunarity;
+                }
+                if(noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                toReturn[x, y] = noiseHeight;
+            }
+        }
+        for (int x = 0; x < width; x++) 
+        {
+            for (int y = 0; y < height; y++)
+            {
+                toReturn[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, toReturn[x, y]);
             }
         }
         return toReturn;
